e40e5418d5ff57498356d1d657a51c0a7dfe9508
Comision-21647A-Caballero-Andrea <alorenacab@hotmail.com>
2023-09-29 00:04:16 -0300

    helmet

--- Index.js
+++ Index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const helmet = require ('helmet')
 
 const { DBTest } = require('./Database.js');
 const PublicacionModel = require('./PublicacionModel.js');
@@ -11,6 +12,7 @@ const PUERTO = process.env.PUERTO
 
 app.set('view engine', 'ejs');
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
